afb1e20b45560705acb1af8a9d8a5003367482ed
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-10-27 01:19:57 +0000

    feat: add lazy plugin manager and autopairs

--- nvim/lua/core/opts.lua
+++ nvim/lua/core/opts.lua
@@ -2,4 +2,11 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+opt.cursorline = true
+opt.termguicolors = true
+opt.splitright = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true 
 opt.swapfile = false
+
